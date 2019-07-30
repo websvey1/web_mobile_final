98f99df54768a89426f79ef7320b59f387e90084
송영신 <youngsin1990@kut.ac.kr>
2019-07-30 13:14:48 +0900

    sorry

--- technology-blog/src/views/PostReadPage.vue
+++ technology-blog/src/views/PostReadPage.vue
@@ -1,41 +1,4 @@
 <template>
-<<<<<<< HEAD
-<div style="display: flex; justify-content: center;">
-  <v-app id="inspire">
-
-    <!-- 프로필, 작성일 -->
-
-    <div style="margin-bottom:70px;">
-      <h1 style="text-align:center;">{{post.title}}</h1>
-    </div>
-
-    <div>
-        <h3 style="text-align:left; margin-bottom:10px;">{{post.writer}} </h3>
-        <h3 style="text-align:left; margin-bottom:10px;">{{post.wdate}}</h3>
-    </div>
-    <!-- 사진 -->
-    <div>
-      <v-img :src="post.imageUrl" aspect-ratio="1.7" class="grey darken-4" position="center">
-      </v-img>
-    </div>
-
-    <div class="content-field">
-      <h3 style="text-align:center; margin: 50px 0;">{{post.content}}</h3>
-    </div>
-
-    <div style="text-align:center;" id="write-btn">
-      <v-chip v-for="tag in post.tags" outline :color="tag.color">
-        #{{tag.text}}
-      </v-chip>
-
-      <v-btn class="v-btn theme--dark" @click="goHome">목록</v-btn>
-      <v-btn v-if="modify" class="v-btn theme--dark" @click="goUpdate">수정</v-btn>
-    </div>
-
-
-  </v-app>
-</div>
-=======
 <v-layout wrap align-center justify-center row fill-height>
   <v-flex xs12 text-xs-left>
     <v-img v-if="post != null" :src="post.image_url" aspect-ratio="1.7">
@@ -63,9 +26,7 @@
     </v-img>
   </v-flex>
 </v-layout>
->>>>>>> 67c37e570a865306815febc50d1103db2605c049
 </template>
-
 
 <script>
 import markdownEditor from 'vue-simplemde/src/markdown-editor'
@@ -81,56 +42,21 @@ export default {
   },
   data() {
     return {
-      post: null,
-      tags: [],
-      modify: false
+      post:null,
+      tags:[],
+      modify:false
     }
   },
   methods: {
-    goHome() {
+    goHome(){
       this.$router.push("/post")
     },
-    goUpdate() {
-      this.$router.push({
-        name: "PostUpdatePage",
-        params: {
-          id: this.$route.params.id,
-          email: this.post.email
-        }
-      })
+    goUpdate(){
+      this.$router.push({name:"PostUpdatePage", params:{id:this.$route.params.id, email:this.post.email}})
     },
-    async fetchData() {
+    async fetchData(){
       this.post = null;
 
-<<<<<<< HEAD
-      console.log(this.$route.params.id);
-      await FirebaseService.readPost(this.$route.params.id).then(async data => {
-        console.log("data : " + data);
-        if (data == null) {
-          alert("게시글이 존재하지 않습니다.");
-          this.goHome();
-          return;
-        }
-
-        this.post = data;
-        console.log("왜 안되 씨발");
-        var user = this.$store.state.userInfo;
-
-        console.log(user);
-        if (user != null) {
-          if (user.email == this.post.email) {
-            this.modify = true
-          }
-        }
-      })
-    }
-  },
-  async created() {
-    if (this.$store.state.userInfo != null) {
-      LogService.CreatedTime(this);
-    }
-    console.log("log : " + await FirebaseService.getUser());
-=======
       this.$http.get("http://192.168.31.65:3000/post/read/" + this.$route.params.id)
       .then((response) => {
         this.post = response.data.post
@@ -154,26 +80,38 @@ export default {
     }
   },
   async created(){
->>>>>>> 67c37e570a865306815febc50d1103db2605c049
     await this.fetchData();
   },
-  beforeRouteLeave(to, from, next) {
-    if (this.$store.state.logInfo != null) {
+  beforeRouteLeave(to, from, next){
+    if(this.$store.state.logInfo != null){
       LogService.DestroyedTime(this);
     }
     next();
   },
-  destroyed() {
+  destroyed(){
 
   }
 
 }
 </script>
-<style>
-  #inspire{
-    padding-top: 100px;
-    width:50%;
-    display: flex;
-    justify-content: center;
-  }
+
+<style scoped>
+#preview {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#preview img {
+  max-width: 100%;
+  max-height: 200px;
+}
+
+#btn {
+  float: center;
+}
+
+.content-field{
+  min-height: 500px;
+}
 </style>
