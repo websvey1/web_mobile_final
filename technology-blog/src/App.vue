578e3ab8bf7484b2d7fbaa84e8839f07cc5fa28c
송영신 <youngsin1990@kut.ac.kr>
2019-07-30 11:24:51 +0900

    Header & PostReadPage

--- technology-blog/src/App.vue
+++ technology-blog/src/App.vue
@@ -1,85 +1,37 @@
 <template>
   <div>
-    <HeaderTest v-if="$router.currentRoute.name != 'HomePage'"/>
+    <!-- <HeaderTest v-if="$router.currentRoute.name != 'HomePage'"/> -->
+    <HeaderTest/>
     <v-app>
       <v-content>
         <router-view/>
       </v-content>
     </v-app>
-
-    <div id="app">
-      <div class="main">
-        <div class="calendar-holder">
-          <calendar :events="events" />
-        </div>
-
-        <div class="form-holder">
-          <h3>Schedule an event</h3>
-          <event-form />
-        </div>
-      </div>
-    </div>
     <Footer/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
+// import Header from './components/Header'
 import Footer from './components/Footer'
 import FirebaseService from './services/FirebaseService'
 import HeaderTest from './components/HeaderTest'
-
-import Calendar from './components/Calendar.vue'
-import EventForm from './components/EventForm.vue'
-import Pusher from 'pusher-js';
-import {mapState} from 'vuex';
 
 export default {
   name: 'App',
   components: {
     Footer,
-    Header,
-    HeaderTest,
-    Calendar,
-    EventForm
+
+    HeaderTest
   },
   data () {
     return {
       //
-      // events: [
-      //   {
-      //     title     :  'event1',
-      //     start     : '2019-07-09',
-      //     cssClass  : 'blue',
-      //     YOUR_DATA : {}
-      //   },
-      //   {
-      //     title     : 'event2',
-      //     start     : '2019-07-10',
-      //     end       : '2019-07-13',
-      //     cssClass  : ['orange']
-      //   }
-      // ] 
-      events: [{
-        title     : 'event2',
-        start     : '2019-07-10',
-        end       : '2019-07-13',
-        cssClass  : ['orange']
-      }]
-      // events: []
     }
   },
 
   created(){
-    console.log(this.events);
-    var config = {
-      id : 'hong',
-      pwd: '12345',
-      dept_no : 1,
-      user_id : 1
-    }
-
-    this.$http.post("/dept",config)
+    this.$http.post("/dept/1")
     .then(res => {
       alert(res.data)
       console.log(res.data);
@@ -88,68 +40,16 @@ export default {
       // alert(error)
       console.log("안됐지롱");
     })
-
   },
   methods:{
     async readPosts(){
       await FirebaseService.readPosts();
     }
   },
+
   mounted(){
-    this.readPosts();
-
-    this.$http.post('http://192.168.31.63:3000/getPlan')
-      .then((response) => {
-        var items = response.body;
-        console.log(items)
-        for(var i = 0; i < items.length; i++){
-          this.events.push({title: items[i].cal_title, start: items[i].cal_start, 
-                  end: items[i].cal_end, cssClass: items[i].cal_color, description: items[i].cal_description});
-        }
-
-        console.log(this.events)
-      })
-      .catch((error) =>{
-        console.log(error)
-      })
-  },
-  computed:
-    mapState(['plan'])
-  ,
-  watch: {
-    plan(to,from){
-      this.events.push(this.$store.state.plan);
-    }
-  },
+    this.readPosts()
+  }
 
 }
 </script>
-
-<style>
-    #app {
-      font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: center;
-      color: #2c3e50;
-      margin-top: 60px;
-    }
-    .main {
-      display: flex;
-      align-items: center;
-    }
-    .calendar-holder {
-      width: 65%;
-    }
-    .form-holder {
-      width: 35%;
-    }
-    .form-holder > h3 {
-      color: orangered;
-      text-transform: uppercase;
-      font-size: 16px;
-      text-align: left;
-      margin-left: 30px;
-      margin-bottom: 10px;
-    }
-    </style>
--- technology-blog/src/views/PostReadPage.vue
+++ technology-blog/src/views/PostReadPage.vue
@@ -1,32 +1,41 @@
 <template>
-<v-layout wrap align-center justify-center row fill-height>
-  <v-flex xs12 text-xs-left>
-    <v-img v-if="post != null" :src="post.imageUrl" aspect-ratio="1.7">
-      <v-layout wrap pa-5 fill-height>
-        <v-flex pa-5 ma-5 white xs12>
-          <h1>{{post.title}}</h1>
-          <h3 style="text-align:right;color:grey;">작성자 : {{post.writer}}</h3>
-          <v-divider></v-divider>
-          <h3 style="text-align:right;color:grey;">{{post.wdate}}</h3>
-          <v-divider dark></v-divider>
-          <div class="content-field">
-            <h3>{{post.content}}</h3>
-          </div>
-          <div style="text-align:center;" id="write-btn">
-            <v-chip v-for="tag in post.tags" outline :color="tag.color" >
-               #{{tag.text}}
-             </v-chip>
+<div style="display: flex; justify-content: center;">
+  <v-app id="inspire">
 
-            <v-divider dark></v-divider>
-            <v-btn class="v-btn theme--dark" @click="goHome">목록</v-btn>
-            <v-btn v-if="modify" class="v-btn theme--dark" @click="goUpdate">수정</v-btn>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-img>
-  </v-flex>
-</v-layout>
+    <!-- 프로필, 작성일 -->
+
+    <div style="margin-bottom:70px;">
+      <h1 style="text-align:center;">{{post.title}}</h1>
+    </div>
+
+    <div>
+        <h3 style="text-align:left; margin-bottom:10px;">{{post.writer}} </h3>
+        <h3 style="text-align:left; margin-bottom:10px;">{{post.wdate}}</h3>
+    </div>
+    <!-- 사진 -->
+    <div>
+      <v-img :src="post.imageUrl" aspect-ratio="1.7" class="grey darken-4" position="center">
+      </v-img>
+    </div>
+
+    <div class="content-field">
+      <h3 style="text-align:center; margin: 50px 0;">{{post.content}}</h3>
+    </div>
+
+    <div style="text-align:center;" id="write-btn">
+      <v-chip v-for="tag in post.tags" outline :color="tag.color">
+        #{{tag.text}}
+      </v-chip>
+
+      <v-btn class="v-btn theme--dark" @click="goHome">목록</v-btn>
+      <v-btn v-if="modify" class="v-btn theme--dark" @click="goUpdate">수정</v-btn>
+    </div>
+
+
+  </v-app>
+</div>
 </template>
+
 
 <script>
 import markdownEditor from 'vue-simplemde/src/markdown-editor'
@@ -42,80 +51,73 @@ export default {
   },
   data() {
     return {
-      post:null,
-      tags:[],
-      modify:false
+      post: null,
+      tags: [],
+      modify: false
     }
   },
   methods: {
-    goHome(){
+    goHome() {
       this.$router.push("/post")
     },
-    goUpdate(){
-      this.$router.push({name:"PostUpdatePage", params:{id:this.$route.params.id, email:this.post.email}})
+    goUpdate() {
+      this.$router.push({
+        name: "PostUpdatePage",
+        params: {
+          id: this.$route.params.id,
+          email: this.post.email
+        }
+      })
     },
-    async fetchData(){
+    async fetchData() {
       this.post = null;
 
       console.log(this.$route.params.id);
       await FirebaseService.readPost(this.$route.params.id).then(async data => {
-          console.log("data : " + data);
-          if(data == null){
-            alert("게시글이 존재하지 않습니다.");
-            this.goHome();
-            return;
-          }
+        console.log("data : " + data);
+        if (data == null) {
+          alert("게시글이 존재하지 않습니다.");
+          this.goHome();
+          return;
+        }
 
-          this.post = data;
-          console.log("왜 안되 씨발");
-          var user = this.$store.state.userInfo;
+        this.post = data;
+        console.log("왜 안되 씨발");
+        var user = this.$store.state.userInfo;
 
-          console.log(user);
-          if(user != null){
-            if(user.email == this.post.email){
-              this.modify = true
-            }
+        console.log(user);
+        if (user != null) {
+          if (user.email == this.post.email) {
+            this.modify = true
           }
-        })
+        }
+      })
     }
   },
-  async created(){
-    if(this.$store.state.userInfo != null){
+  async created() {
+    if (this.$store.state.userInfo != null) {
       LogService.CreatedTime(this);
     }
     console.log("log : " + await FirebaseService.getUser());
     await this.fetchData();
   },
-  beforeRouteLeave(to, from, next){
-    if(this.$store.state.logInfo != null){
+  beforeRouteLeave(to, from, next) {
+    if (this.$store.state.logInfo != null) {
       LogService.DestroyedTime(this);
     }
     next();
   },
-  destroyed(){
+  destroyed() {
 
   }
 
 }
 </script>
-
-<style scoped>
-#preview {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-#preview img {
-  max-width: 100%;
-  max-height: 200px;
-}
-
-#btn {
-  float: center;
-}
-
-.content-field{
-  min-height: 500px;
-}
+<style>
+  #inspire{
+    padding-top: 100px;
+    width:50%;
+    display: flex;
+    justify-content: center;
+  }
 </style>
