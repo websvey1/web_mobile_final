689daa55b26665d43d20ef56ff520883a018162b
김예지 <dpwl7484@gmail.com>
2019-07-31 19:11:48 +0900

    yeji

--- technology-backend/routes/user.js
+++ technology-backend/routes/user.js
@@ -29,7 +29,63 @@ router.post('/login', function(req, res, next) {
   });
 });
 
-router.post('/adduser', function(req, res, next) {
+router.post('/create', function(req, res, next) {
+  var pool = db.getPool();
+  var id = req.body.id;
+  var password = req.body.password;
+  var email = req.body.password;
+  var name = req.body.name;
+  var favor = req.body.favor;
+
+  console.log(req.body);
+  pool.getConnection((ex, conn) => {
+    if(ex){
+      console.log(ex);
+    }
+    else{
+      var sql = 'insert into user(user_id, user_password, user_email, user_name) values("'+id+'","' +password+'","'+email+'","' +name + '")';
+      var query = conn.query(sql,function (err, result) {
+        if (err) {
+          console.error(err);
+          conn.release();
+          throw err;
+        }
+
+        var user_num = result.insertId;
+
+        if(favor.length > 0){
+          var sql = "insert into favor(favor_user, favor_tech) values"
+
+          for(var i = 0; i < favor.length; i++){
+            sql += "("+user_num+"," + favor[i] +")"
+            if(i + 1 == favor.length){
+              sql += ";"
+            }
+            else{
+              sql += ","
+            }
+          }
+
+          var querry = conn.query(sql,function (err, result) {
+            if (err) {
+              console.error(err);
+              throw err;
+            }
+
+            res.send("Success");
+          })
+        }
+
+        else{
+          res.send("Success");
+        }
+      });
+    }
+    conn.release();
+  });
+});
+
+router.put('/update', function(req, res, next) {
   var pool = db.getPool();
   var id = req.body.id;
   var password = req.body.password;
@@ -50,6 +106,56 @@ router.post('/adduser', function(req, res, next) {
         }
 
         res.send("Success");
+      });
+    }
+    conn.release();
+  });
+});
+
+router.delete('/delete', function(req, res, next) {
+  var pool = db.getPool();
+  var id = req.body.id;
+  var password = req.body.password;
+  var email = req.body.password;
+  var name = req.body.name;
+
+  pool.getConnection((ex, conn) => {
+    if(ex){
+      console.log(ex);
+    }
+    else{
+      var query = conn.query('insert into user(user_id, user_password, user_email, user_name) values("'+id+'","' +password+'","'+email+'","' +name + '")',
+        function (err, result) {
+        if (err) {
+          console.error(err);
+          conn.release();
+          throw err;
+        }
+
+        res.send("Success");
+      });
+    }
+    conn.release();
+  });
+});
+
+router.get('/tech', function(req, res, next) {
+  var pool = db.getPool();
+
+  pool.getConnection((ex, conn) => {
+    if(ex){
+      console.log(ex);
+    }
+    else{
+      var query = conn.query('select * from tech',
+        function (err, result) {
+        if (err) {
+          console.error(err);
+          conn.release();
+          throw err;
+        }
+
+        res.send(result);
       });
     }
     conn.release();
