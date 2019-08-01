f25096fb1f98b7eb867566a9569523c847f839cf
JunhoKim <rlawnsgh61@gmail.com>
2019-08-01 10:22:14 +0900

    personal calendar complete

--- technology-blog/src/App.vue
+++ technology-blog/src/App.vue
@@ -29,42 +29,20 @@
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
 
-import Calendar from './components/Calendar.vue'
-import EventForm from './components/EventForm.vue'
-import Pusher from 'pusher-js';
-import {mapState} from 'vuex';
-
 export default {
   name: 'App',
   components: {
     Footer,
     Header,
-    Calendar,
-    EventForm
   },
   data () {
     return {
-      //
-      // events: [
-      //   {
-      //     title     :  'event1',
-      //     start     : '2019-07-09',
-      //     cssClass  : 'blue',
-      //     YOUR_DATA : {}
-      //   },
-      //   {
-      //     title     : 'event2',
-      //     start     : '2019-07-10',
-      //     end       : '2019-07-13',
-      //     cssClass  : ['orange']
-      //   }
-      // ]
-      events: [{
-        title     : 'event2',
-        start     : '2019-07-10',
-        end       : '2019-07-13',
-        cssClass  : ['orange']
-      }]
+      // events: [{
+      //   title     : 'event2',
+      //   start     : '2019-07-10',
+      //   end       : '2019-07-13',
+      //   cssClass  : ['orange']
+      // }]
       // events: []
     }
   },
@@ -76,27 +54,27 @@ export default {
 
   },
   mounted(){
-    this.$http.post('http://192.168.31.63:3000/getPlan')
-      .then((response) => {
-        var items = response.body;
-        // console.log(items)
-        for(var i = 0; i < items.length; i++){
-          this.events.push({title: items[i].cal_title, start: items[i].cal_start,
-                  end: items[i].cal_end, cssClass: items[i].cal_color, description: items[i].cal_description});
-        }
-      })
-      .catch((error) =>{
-        // console.log(error)
-      })
+    // this.$http.post('http://192.168.31.63:3000/getPlan')
+    //   .then((response) => {
+    //     var items = response.body;
+    //     // console.log(items)
+    //     for(var i = 0; i < items.length; i++){
+    //       this.events.push({title: items[i].cal_title, start: items[i].cal_start,
+    //               end: items[i].cal_end, cssClass: items[i].cal_color, description: items[i].cal_description});
+    //     }
+    //   })
+    //   .catch((error) =>{
+    //     // console.log(error)
+    //   })
   },
-  computed:
-    mapState(['plan'])
-  ,
-  watch: {
-    plan(to,from){
-      this.events.push(this.$store.state.plan);
-    }
-  },
+  // computed:
+  //   mapState(['plan'])
+  // ,
+  // watch: {
+  //   plan(to,from){
+  //     this.events.push(this.$store.state.plan);
+  //   }
+  // },
 
 }
 </script>
