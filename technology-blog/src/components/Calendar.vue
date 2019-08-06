fd7d36410b0299031c3ae2d0ac8d5f12ab572e5c
JunhoKim <rlawnsgh61@gmail.com>
2019-08-06 10:13:33 +0900

    calendar modify function -> start date <= end date

--- technology-blog/src/components/Calendar.vue
+++ technology-blog/src/components/Calendar.vue
@@ -110,19 +110,26 @@ export default {
         })
     },
     modify() {
+      var start = format(this.start, 'YYYY-MM-DD');
+      var end = format(this.end, 'YYYY-MM-DD');
+      if(start > end){
+        alert("Start date should be earlier than End date")
+        return
+      }
+
       var data = {
         id: this.$store.state.cal_id,
         title: this.title,
-        start: format(this.start, 'YYYY-MM-DD'),
-        end: format(this.end, 'YYYY-MM-DD'),
+        start: start,
+        end: end,
         description: this.description,
         cssClass: this.cssClass
       }
 
       this.$http.post('http://192.168.31.63:3000/plan/modifyPersonal', data)
         .then((response) => {
+          this.$store.state.plan = true;
           this.dialog = false
-          location.reload()
         })
         .catch((error) => {
           console.log(error)
